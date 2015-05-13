b7c6044a5668890895b470c53e9a94921b672d21
unknown <intern@Emooth112-PC.emooth.com>
2015-05-13 10:26:38 +1000

    + Plugin information + Preload CSS

--- functions.php
+++ functions.php
@@ -1,17 +1,38 @@
-<?php // Opening PHP tag - nothing should be before this, not even whitespace
+<?php
+/*
+Plugin Name: zRolling
+Plugin URI: N/A
+Description: Recent news rolling
+Version: 0.1
+Author: Zac
+Author URI: N/A
+*/
+?>
 
-// Custom Function to Include
+<?php
+/*  Copyright 2015  Zac  (email : zacfirst@gmail.com)
 
-// function rolling() {
-//     echo '<script src="' . get_stylesheet_directory_uri() . '/rolling.js"></script>' . "\n";
-// }
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
 
-// add_action( 'wp_head', 'rolling' );
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+?>
 
+<?php
 function my_theme_scripts_function1() {
-	wp_enqueue_script( 'zRolling', get_stylesheet_directory_uri() . '/rolling.js');
-	wp_enqueue_script( 'marquee', get_stylesheet_directory_uri() . '/jquery.marquee.min.js');
-}	
+    wp_enqueue_style( 'style.css', plugins_url() . '/zRolling/style.css' ); 
+	wp_enqueue_script( 'zRolling', plugins_url() . '/zRolling/rolling.js');
+	wp_enqueue_script( 'marquee', plugins_url() . '/zRolling/jquery.marquee.min.js');
+}
 
 add_action('wp_enqueue_scripts','my_theme_scripts_function1', 11);
